26b7c5d87accc77575d154da67182f8843d5161a
elena svensson <lena_reo@hotmail.com>
2017-12-29 10:04:07 +0100

    Setting background

--- CatchGame/Window1.cs
+++ CatchGame/Window1.cs
@@ -11,5 +11,31 @@ namespace CatchGame
 {
     class Window1 : Form
     {
+        public Window1()
+        {
+            TableLayoutPanel panel = new TableLayoutPanel
+            {
+            ColumnCount = 2,
+            Dock = DockStyle.Fill
+        };
+            Controls.Add(panel);
+
+            PictureBox gameBox = new PictureBox
+            {
+                Dock = DockStyle.Fill
+            };
+            panel.Controls.Add(gameBox);
+
+            Label score = new Label
+            {
+                Text = "Score"
+            };
+            panel.Controls.Add(score);
+
+        }
+       
+        
+        
+
     }
 }
