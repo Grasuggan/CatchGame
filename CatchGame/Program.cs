6decf7161fecb78434a591f363107013ef53eae9
elena svensson <lena_reo@hotmail.com>
2017-12-29 09:56:03 +0100

    Starting game program

---
+++ CatchGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace CatchGame
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Window1());
+        }
+    }
+}
---
+++ CatchGame/Window1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace CatchGame
+{
+    class Window1 : Form
+    {
+    }
+}
