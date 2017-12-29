b78763fb038e3a261dec40f4cb312417b84bf1f7
elena svensson <lena_reo@hotmail.com>
2017-12-29 11:43:55 +0100

    Adding som items

---
+++ CatchGame/Window2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace CatchGame
+{
+    class Window2 : Window1
+    {
+
+        public Window2()
+        {
+
+        }
+    }
+}
